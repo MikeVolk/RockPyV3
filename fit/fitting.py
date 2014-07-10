044f85093515edcc2845e9c18ca594bad5a88117
mike_home <grossstadtlegende@gmail.com>
2014-07-10 17:15:57 +0200

    Sphinx added, new functions added. Massive changes in plotting new plotting functions

--- fit/fitting.py
+++ fit/fitting.py
@@ -1,16 +1,23 @@
 __author__ = 'Mike'
+import logging
 import numpy as np
-from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
 import math as math
 import scipy.special as sp
-import distributions
+import distributions, functions
 from lmfit import Parameters, minimize, printfuncs
 import matplotlib.pyplot as plt
+from RockPyV3.Functions import general
+from scipy.optimize import curve_fit
+
+
 class Fit(object):
-    def __init__(self, x_data, y_data, paramters = None):
-        self.x_data = x_data
-        self.y_data = y_data
+    def __init__(self, x_data, y_data, paramters=None, log=None):
+        if not log:
+            self.log = logging.getLogger('RockPy.FITTING')
+
+        self.x = x_data
+        self.y = y_data
         
         if not paramters:
             self.parameters = Parameters()
@@ -26,6 +33,56 @@ class HyperbolicBase(Fit):
 
     def __init__(self, x_data, y_data, paramters = None):
         super(HyperbolicBase, self).__init__(x_data, y_data, paramters = None)
+
+
+class Tanh(Fit):
+    general.create_logger('RockPy.FITTING.tanh')
+
+    def __init__(self, x_data, y_data, paramters=None):
+        self.log = logging.getLogger('RockPy.FITTING.TANH')
+        super(Tanh, self).__init__(x_data, y_data, paramters=None, log=self.log)
+
+        self.fit_x = np.arange(min(self.x), max(self.x), 0.01)
+
+        amp = np.max(abs(self.y))
+        zero_idx = np.argmin(abs(self.y))
+
+        self.parameters.add('amp', value=amp)
+        self.parameters.add('shift', value=self.x[zero_idx])
+
+        ''' Fitting '''
+        errfunc = lambda p, x, y: functions.func_tanh(self.parameters, x) - y
+
+        fitout = minimize(errfunc, self.parameters, args=(self.x, self.y))
+
+        self.fit_y = functions.func_tanh(self.parameters, self.fit_x)
+
+    def calc(self, x_value):
+        out = functions.func_tanh(self.parameters, x_value)
+        return out
+
+
+class Log_Normal(Fit):
+    general.create_logger('RockPy.FITTING.log_normal')
+
+    def __init__(self, x_data, y_data, paramters=None):
+        self.log = logging.getLogger('RockPy.FITTING.TANH')
+        super(Log_Normal, self).__init__(x_data, y_data, paramters=None, log=self.log)
+        self.log.info('FITTING\t log normal distribution to data')
+        self.fit_x = np.linspace(min(self.x), max(self.x), 1000)
+        self.parameters.add('B', value=0.01)
+        self.parameters.add('E', value=1.0)
+        self.parameters.add('G', value=0.5)
+
+        ''' Fitting '''
+        errfunc = lambda p, x, y: functions.log_normal(self.parameters, x) - y
+        fitout = minimize(errfunc, self.parameters, args=(self.x, self.y))
+
+        self.fit_y = functions.log_normal(self.parameters, self.fit_x)
+
+    def calc(self, x_value):
+        out = functions.log_normal(self.parameters, x_value)
+        return out
 
 
 def normal_skewed(x, y, parameters=None, dfunc='pdf', check=False, *args, **kwargs):
@@ -44,6 +101,7 @@ def normal_skewed(x, y, parameters=None, dfunc='pdf', check=False, *args, **kwar
         reverse = True
     else:
         reverse = False
+
     errfunc = lambda p, x, y: distributions.normal_skew(x, parameters, reverse=reverse, dfunc=dfunc) - y
     fitout = minimize(errfunc, parameters, args=(x, y))
 
--- fit/functions.py
+++ fit/functions.py
@@ -1,7 +1,9 @@
 __author__ = 'mike'
 import numpy as np
+from scipy import special
 
-def hyperbolic_basis(x, parameters):
+
+def hyperbolic_basis(parameters, x):
     '''
     calculation for fit of hyperbolic basis function according to von Dobeneck 1995
     typically only works on saturated samples
@@ -17,3 +19,33 @@ def hyperbolic_basis(x, parameters):
 
 
     func1 = a1 * np.tanh()
+
+
+def func_tanh(parameters, x):
+    '''
+    '''
+    amp = parameters['amp'].value
+    shift = parameters['shift'].value
+    out = -amp * np.tanh(x + shift)
+    return out
+
+
+def log_normal(parameters, x):
+    '''
+
+    :math:`E_i` represents the mean value of a correspond- ing log-Gaussian distribution, and therefore, the
+    logarithmic field value of the maximum gradient.
+    :math:`G_i` describes the standard deviation or half-width of the distribution
+
+    math::
+
+       M_{\text{fit}} = B * G * \sqrt{\frac{\pi}{2} \erf (\frac{(x-E)}{G}) - erf(\frac{-E}{G})
+    '''
+    B = parameters['B'].value
+    E = parameters['E'].value
+    G = parameters['G'].value
+
+    pi = np.pi
+
+    out = B * G * ( np.sqrt(pi) / 2 ) * ( special.erf((x - E) / G) - special.erf(-E / G) )
+    return out
