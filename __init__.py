fcaa750fe47b8c91a1f8c49d1d0b60fcc0ff06a6
mike_home <grossstadtlegende@gmail.com>
2014-07-10 17:11:39 +0200

    Sphinx added, new functions added. Massive changes in plotting new plotting functions

--- __init__.py
+++ __init__.py
@@ -1,4 +1,7 @@
 __author__ = 'Mike'
 import helper
-import Functions
-import ReadIn
+# import Functions
+# import ReadIn
+from Structure.sample import Sample
+from Structure.sample import SampleGroup
+from Structure.sample import sample_import
