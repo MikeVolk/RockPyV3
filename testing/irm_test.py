525b7c00188b2be9a0b37d51d9ec62532e156eb3
mike_home <grossstadtlegende@gmail.com>
2014-09-12 14:18:31 +0200

    Added IRM-unmixing plot - not finished

--- testing/irm_test.py
+++ testing/irm_test.py
@@ -6,4 +6,4 @@ data = '../test_data/irm_test.irm'
 S = Sample(name='irm_test')
 M = S.add_measurement(mtype='irm', mfile=data, machine='vsm')
 
-M.plt_irm_derivative(smoothing=1, norm=True)
+M.plt_irm_unmixing(smoothing=1, norm=False)
